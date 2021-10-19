2815ef796b7e3027074a539e18fa13094e12a2ae
FMRedondo <65115094+FMRedondo@users.noreply.github.com>
2021-10-19 12:32:03 +0200

    tailwind compilado

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ManfredVC\Documents\GitHub\ClickMS\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ManfredVC\Documents\GitHub\ClickMS\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\FMRedondo\Desktop\clickMS\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\FMRedondo\Desktop\clickMS\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
