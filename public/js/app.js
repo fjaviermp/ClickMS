0ad89e567cd96e5cc8e0670965ffc926c832d2b5
FMRedondo <65115094+FMRedondo@users.noreply.github.com>
2021-10-19 12:54:49 +0200

    arreglo errores de instalacion

--- public/js/app.js
+++ public/js/app.js
@@ -49836,17 +49836,6 @@ module.exports = function(module) {
 
 /***/ }),
 
-/***/ "./resources/css/app.css":
-/*!*******************************!*\
-  !*** ./resources/css/app.css ***!
-  \*******************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nError: PostCSS plugin tailwindcss requires PostCSS 8.\nMigration guide for end-users:\nhttps://github.com/postcss/postcss/wiki/PostCSS-8-for-end-users\n    at Processor.normalize (C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\postcss-loader\\node_modules\\postcss\\lib\\processor.js:153:15)\n    at new Processor (C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\postcss-loader\\node_modules\\postcss\\lib\\processor.js:56:25)\n    at postcss (C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\postcss-loader\\node_modules\\postcss\\lib\\postcss.js:55:10)\n    at C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\postcss-loader\\src\\index.js:140:12\n    at runMicrotasks (<anonymous>)\n    at processTicksAndRejections (internal/process/task_queues.js:95:5)\n    at C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\Users\\ManfredVC\\Documents\\GitHub\\ClickMS\\node_modules\\postcss-loader\\src\\index.js:208:9\n    at runMicrotasks (<anonymous>)\n    at processTicksAndRejections (internal/process/task_queues.js:95:5)");
-
-/***/ }),
-
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -49999,20 +49988,26 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!***********************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/css/app.css ***!
-  \***********************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 __webpack_require__(/*! C:\Users\FMRedondo\Desktop\clickMS\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\FMRedondo\Desktop\clickMS\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Users\ManfredVC\Documents\GitHub\ClickMS\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ManfredVC\Documents\GitHub\ClickMS\resources\css\app.css */"./resources/css/app.css");
->>>>>>> 4895562f219976240430967f446118f066fd6469
 
 
 /***/ })
