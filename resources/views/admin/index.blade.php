6b6480612233193a05a5124743a29dee263e644f
FMRedondo <65115094+FMRedondo@users.noreply.github.com>
2021-10-21 00:17:24 +0200

    laravel y paquetes actualizados

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,8 +11,7 @@
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/adminlte.css">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
